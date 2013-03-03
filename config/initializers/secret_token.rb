50b95354bd9d7c365907a62720964cfa6827275c
Adam Krebs <amk528@cs.nyu.edu>
2013-03-03 01:38:05 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Copenhagen::Application.config.secret_token = '0a8b500ff3730f03490ff4065d963ca9640cf48c2ffca48df254cd017238c9adb90b8b8b40b6913d764058f9eea5435fea1b0699de97ce5f61c13d41a233c27e'
