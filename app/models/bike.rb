52064f89b4d8e49c9b1c99f6bd36fb73ddfc56e8
Adam Krebs <amk528@cs.nyu.edu>
2013-03-04 20:45:13 -0500

    condition validation

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,5 +1,7 @@
 class Bike < ActiveRecord::Base
   attr_accessible :color, :condition, :location_id, :image_url, :manufacturer, :model
 
+  validates_inclusion_of :condition, in: %w( operational offline )
+
   belongs_to :location
 end
