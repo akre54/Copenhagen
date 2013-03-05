1c0df39f3dfca53a087d02e07f80c1222577966f
Eric Sluyter <wondersluyter@gmail.com>
2013-03-04 22:49:28 -0500

    fucked

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,7 +1,7 @@
 class Bike < ActiveRecord::Base
   attr_accessible :color, :condition, :location_id, :image_url, :manufacturer, :model
 
-  validates_inclusion_of :condition, in: %w( operational offline )
+  validates_inclusion_of :condition, in: %w( fucked operational offline )
 
   belongs_to :location
 end
