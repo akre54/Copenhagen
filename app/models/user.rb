48e4ebe58baa79849191759ec33485ffacc83b01
Adam Krebs <amk528@cs.nyu.edu>
2013-03-04 21:42:50 -0500

    use an AFFILIATIONS constant

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :email_address, :first_name, :last_name, :netid, :affilition, :year_of_graduation
 
-  validates_inclusion_of :affilition, in: %w(undergraduate graduate faculty staff)
+
+  AFFILIATIONS = %w(undergraduate graduate faculty staff).freeze
+
+  validates_inclusion_of :affilition, in: AFFILIATIONS
 end
