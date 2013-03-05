b8b92f3ba17b59f8aafa055d2a3726732dbc1001
Adam Krebs <amk528@cs.nyu.edu>
2013-03-04 21:32:54 -0500

    set up user type

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :email_address, :first_name, :last_name, :netid, :type, :year_of_graduation
+
+  validates_inclusion_of :type, in: %w(undergraduate graduate faculty staff)
 end
