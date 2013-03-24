d9fc4d13e53b044ea70bd27a783f3549a18181cc
Adam Krebs <amk528@cs.nyu.edu>
2013-03-23 21:26:39 -0400

    make devise happy

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Copenhagen::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Make devise happy; options for email resets, etc.
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
