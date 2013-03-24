beaa072f37f41b64265c6d363bfd28191dfa208c
Adam Krebs <amk528@cs.nyu.edu>
2013-03-24 17:27:39 -0400

    checkin + checkout bikes

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -9,6 +9,16 @@ class Bike < ActiveRecord::Base
 
   has_many :checkouts
 
+  def checkout(user)
+    self.checkouts << Checkout.new(user: user, location: self.location)
+  end
+
+  def checkin
+    last_checkout = self.checkouts.where(returned_at: nil).limit(1).first
+    last_checkout.returned_at = Time.now
+    last_checkout.save!
+  end
+
   def checked_out_to
     last_checkout = self.checkouts.where(returned_at: nil).limit(1).first
     last_checkout.user if last_checkout
