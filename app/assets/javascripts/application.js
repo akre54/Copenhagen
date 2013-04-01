669080b3d77cedb7bea2858c9dea2b39a0d10afd
Adam Krebs <amk528@cs.nyu.edu>
2013-04-01 00:54:24 -0400

    move application.js to bower

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
+//= require jquery/jquery
+//= require jquery-ujs/src/rails
 //= require twitter/bootstrap/bootstrap-dropdown
 //= require_tree .
