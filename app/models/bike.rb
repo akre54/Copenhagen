95bbaf4dd8a86af471764e6fcb8e25586765f0e7
Adam Krebs <amk528@cs.nyu.edu>
2013-04-02 15:10:30 -0400

    remove some self.s

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -12,26 +12,31 @@ class Bike < ActiveRecord::Base
   end
 
   def checkin
-    last_checkout = self.active_checkout
+    last_checkout = active_checkout
     return unless last_checkout
     last_checkout.returned_at = Time.now
     last_checkout.save
   end
 
   def active_checkout
-    self.checkouts.checked_out.limit(1).first
+    checkouts.checked_out.limit(1).first
   end
 
   def last_checkout
-    self.checkouts.order('created_at DESC').limit(1).first
+    checkouts.order('created_at DESC').limit(1).first
+  end
+
+  def due_at
+    last_checkout = active_checkout
+    last_checkout.due_at if last_checkout
   end
 
   def checked_out?
-    self.active_checkout != nil
+    active_checkout != nil
   end
 
   def checked_out_to
-    last_checkout = self.active_checkout
+    last_checkout = active_checkout
     last_checkout.biker if last_checkout
   end
 
