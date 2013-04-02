1b4052fb2f0e1b8fca87ae1a694c1d58808110cd
Adam Krebs <amk528@cs.nyu.edu>
2013-04-01 23:34:45 -0400

    rename devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Copenhagen::Application.routes.draw do
   resources :bikers
 
 
-  devise_for :users
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' }
 
   resources :users
 
