91838c66675490a60ca98f61ca0eaf8d575fa5aa
Adam Krebs <amk528@cs.nyu.edu>
2013-04-02 03:21:19 -0400

    put checked_out scope on checkout model, add an overdue scope, and delegate the two to checkout

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,27 +1,25 @@
 class Bike < ActiveRecord::Base
   attr_accessible :color, :condition, :location_id, :image_url, :manufacturer, :model
 
-  validates_inclusion_of :condition, in: %w( fucked operational offline )
-
-  scope :checked_out, joins(:checkouts).where(checkouts: {returned_at: nil})
-
-  belongs_to :location
+  delegate :checked_out, :overdue, to: :checkouts
 
   has_many :checkouts
+  belongs_to :location
+  validates_inclusion_of :condition, in: %w( fucked operational offline )
 
   def checkout_to(biker)
     self.checkouts << Checkout.new(biker: biker, location: self.location)
   end
 
   def checkin
-    @last_checkout = self.active_checkout
-    return unless @last_checkout
-    @last_checkout.returned_at = Time.now
-    @last_checkout.save!
+    last_checkout = self.active_checkout
+    return unless last_checkout
+    last_checkout.returned_at = Time.now
+    last_checkout.save
   end
 
   def active_checkout
-    self.checkouts.where(returned_at: nil).limit(1).first
+    self.checked_out.limit(1).first
   end
 
   def last_checkout
--- app/models/checkout.rb
+++ app/models/checkout.rb
@@ -5,6 +5,8 @@ class Checkout < ActiveRecord::Base
   belongs_to :location
 
   scope :latest, lambda{|n| order(:created_at).limit(n)}
+  scope :checked_out, where(returned_at: nil)
+  scope :overdue, where("returned_at = NULL AND due_at < ?", Time.now)
 
   before_create do
     self.due_at = Date.today + 2.days
