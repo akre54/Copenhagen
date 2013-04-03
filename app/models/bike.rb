1c52d683eff661ae6b201bb00a7581aca594a516
Adam Krebs <amk528@cs.nyu.edu>
2013-04-03 11:30:07 -0400

    move overdue and checked_out to Bike class, and overdue? and checked_out? to instance methods

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,11 +1,12 @@
 class Bike < ActiveRecord::Base
   attr_accessible :color, :condition, :location_id, :image_url, :manufacturer, :model
 
-  delegate :checked_out, :overdue, to: :checkouts
-
   has_many :checkouts
   belongs_to :location
   validates_inclusion_of :condition, in: %w( fucked operational offline )
+
+  scope :checked_out, joins(:checkouts).where({checkouts: {returned_at: nil}})
+  scope :overdue, joins(:checkouts).where("checkouts.returned_at IS NULL AND checkouts.due_at < ?", Time.now)
 
   def checkout_to(biker)
     checkout = Checkout.new(biker: biker, location: self.location)
@@ -37,6 +38,10 @@ class Bike < ActiveRecord::Base
 
   def checked_out?
     active_checkout != nil
+  end
+
+  def overdue?
+    checkouts.overdue != []
   end
 
   def checked_out_to
