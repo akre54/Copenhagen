296c039752a7f591093a566db634321e2d136455
Adam Krebs <amk528@cs.nyu.edu>
2013-04-11 11:56:27 -0700

    throw notes on checkouts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130407205906) do
+ActiveRecord::Schema.define(:version => 20130410172128) do
 
   create_table "bikers", :force => true do |t|
     t.string   "first_name"
@@ -40,15 +40,16 @@ ActiveRecord::Schema.define(:version => 20130407205906) do
   end
 
   create_table "checkouts", :force => true do |t|
-    t.integer  "biker_id",         :null => false
-    t.integer  "bike_id",          :null => false
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
-    t.integer  "location_id",      :null => false
+    t.integer  "biker_id",                         :null => false
+    t.integer  "bike_id",                          :null => false
+    t.datetime "created_at",                       :null => false
+    t.datetime "updated_at",                       :null => false
+    t.integer  "location_id",                      :null => false
     t.datetime "due_at"
     t.datetime "returned_at"
     t.integer  "staffer_id"
     t.boolean  "helmet_requested"
+    t.text     "notes",            :default => "", :null => false
   end
 
   add_index "checkouts", ["bike_id"], :name => "index_checkouts_on_bike_id"
