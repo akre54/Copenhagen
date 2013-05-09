6081448444dc66db0616feed97f3e250b4fc8655
Adam Krebs <amk528@cs.nyu.edu>
2013-05-08 23:09:10 -0400

    make rjs work

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,32 @@
+require.config({
+  baseUrl: "assets",
+  paths: {
+    "jquery": "jquery/jquery",
+    "jqery-ujs": "jquery-ujs/src/rails",
+    "bootstrap-dropdown": "twitter/bootstrap/bootstrap-dropdown",
+    "bootstrap-modal": "twitter/bootstrap/bootstrap-modal",
+    "handlebars": "handlebars",
+    "typeahead": "typeahead.js/dist/typeahead",
+    "underscore": "underscore/underscore",
+    "d3": "d3/d3"
+  },
+  shim: {
+    "jquery-ujs": {
+      deps: ["jquery"]
+    },
+    "bootstrap-dropdown": {
+      deps: ["jquery"]
+    },
+    "bootstrap-modal": {
+      deps: ["jquery"]
+    },
+    "typeahead": {
+      deps: ["jquery"]
+    }
+  }
+})
+
+require(['jquery'], function($) {
+  $(function() {
+  });
+});
